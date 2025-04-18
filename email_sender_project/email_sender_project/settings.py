03e74c49925d6c5a14b547126962c09567eb506b
Harshil Mehta <mharshil009@gmail.com>
2025-04-18 12:28:54 +0530

    All host allowed

--- email_sender_project/email_sender_project/settings.py
+++ email_sender_project/email_sender_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1t83pd9o7gjfl7^rfr$3z8k6-$x*c2^-4@m*-bj*b_5z!d!j9c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
